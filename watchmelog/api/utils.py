7a88ea55930d12364e96fcf76df440dfa28cd8f8
nqrse <jerome.vlarouche@gmail.com>
2018-07-06 20:04:45 -0400

    Rework register player with password and straight mongo serialization

---
+++ watchmelog/api/utils.py
@@ -0,0 +1,58 @@
+import bcrypt
+from mongoengine import (
+    Document,
+    DateTimeField,
+    StringField,
+    FloatField,
+    IntField,
+    ListField,
+    EmbeddedDocumentField,
+    ObjectIdField,
+)
+from typing import List
+
+
+def hash_pass(password: bytes, salt: str = None) -> str:
+    hashed_password: bytes = bcrypt.hashpw(password, salt or bcrypt.gensalt())
+    return hashed_password.decode("utf-8")
+
+
+def mongo_to_dict(obj, black_list: List = None):
+    """
+    Turn a mongoengine object into a jsonible python object
+    """
+
+    if not black_list:
+        black_list = []
+    return_data = []
+
+    if isinstance(obj, Document):
+        return_data.append(("id", str(obj.id)))
+
+    for field_name in obj._fields:
+        if field_name in black_list:
+            continue
+        data = obj._data[field_name]
+
+        if isinstance(obj._fields[field_name], DateTimeField):
+            return_data.append((field_name, str(data.isoformat())))
+
+        elif isinstance(obj._fields[field_name], StringField):
+            return_data.append((field_name, str(data)))
+
+        elif isinstance(obj._fields[field_name], FloatField):
+            return_data.append((field_name, float(data)))
+
+        elif isinstance(obj._fields[field_name], IntField):
+            return_data.append((field_name, int(data)))
+
+        elif isinstance(obj._fields[field_name], ListField):
+            return_data.append((field_name, data))
+
+        elif isinstance(obj._fields[field_name], EmbeddedDocumentField):
+            return_data.append((field_name, mongo_to_dict(data)))
+
+        elif isinstance(obj._fields[field_name], ObjectIdField):
+            return_data.append((field_name, str(data)))
+
+    return dict(return_data)
--- watchmelog/api/v1/views/players.py
+++ watchmelog/api/v1/views/players.py
@@ -1,18 +1,36 @@
+import ujson
+from apistar import types, validators
 from apistar.exceptions import BadRequest
 from mongoengine import NotUniqueError
 
+from watchmelog.api.utils import hash_pass, mongo_to_dict
 from watchmelog.api.v1.models.players import Player
 
 
-def register_player(player: Player.Type) -> Player.Type:
+class RegisterPlayer(types.Type):
+    battletag: str = validators.String(description="Your Battle.net Tag")
+    password: str = validators.String(
+        min_length=12, description="Password used to authenticate with the API."
+    )
+    platform: str = validators.String(
+        enum=["PC", "XBOX", "PS4"], description="Platform you're playing on."
+    )
+    region: str = validators.String(
+        enum=["US", "EU", "ASIA"], description="Region you're playing on."
+    )
+
+
+def register_player(player: RegisterPlayer) -> dict:
     """
-    This is coming from the docstring :D
+    Register a new Overwatch player.
     """
+    player.password = hash_pass(player.password.encode("utf-8"))
     new_player = Player(**player)
     try:
         new_player.save()
     except NotUniqueError as exc:
-        raise BadRequest(detail=f"A player with the battletag {player.battletag} already exists.")
-    print(new_player)
+        raise BadRequest(
+            detail=f"A player with the battletag {player.battletag} already exists."
+        )
 
-    return Player.Type(new_player)
+    return mongo_to_dict(new_player, black_list=['password'])
