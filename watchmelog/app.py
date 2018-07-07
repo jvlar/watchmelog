0992fd766798bea9b4e8b230b6c4a3b178b6a1ed
nqrse <jerome.vlarouche@gmail.com>
2018-07-07 01:17:30 -0400

    Add apistart CORS hook plugin

--- watchmelog/app.py
+++ watchmelog/app.py
@@ -1,4 +1,5 @@
 from apistar import App, Include
+from apistar_cors_hooks import CORSRequestHooks
 from mongoengine import connect
 
 from watchmelog.api import v1
@@ -8,7 +9,7 @@ connect("watchmelog")
 
 routes = [Include("/v1/players", name="v1 players", routes=v1.routes)]
 
-app = App(routes=routes, components=[AuthComponent()])
+app = App(routes=routes, components=[AuthComponent()], event_hooks=[CORSRequestHooks()])
 
 if __name__ == "__main__":
     app.serve("127.0.0.1", 5000, debug=True)
