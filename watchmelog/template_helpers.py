c0e28f64afb64521e83a6188206cb5782ca275ba
nqrse <jerome.vlarouche@gmail.com>
2018-07-08 14:26:43 -0400

    Add formhelper and super basic function for form status

--- watchmelog/template_helpers.py
+++ watchmelog/template_helpers.py
@@ -1,5 +1,10 @@
 from watchmelog.config import app_config
 
 
-def get_curr_season():
-    return app_config.current_season
+def get_curr_season() -> int:
+    return int(app_config.current_season)
+
+
+def form_status_class(form) -> str:
+    if form.errors:
+        return "error"
